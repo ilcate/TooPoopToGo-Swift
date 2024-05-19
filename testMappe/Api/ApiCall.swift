0f0a4db92c29b5d4afd98ce99bf6728c3834e525
ilcate <124363106+ilcate@users.noreply.github.com>
2024-05-19 16:42:58 +0200

    poche cose

--- testMappe/Api/ApiCall.swift
+++ testMappe/Api/ApiCall.swift
@@ -102,5 +102,39 @@ class ApiManager: ObservableObject {
                     }
                 }
         }
+    // questo codice dovrebbe essere usato per mandare le immagini
+//    func uploadImage(imgType:String,imgData:Data,imageName:String){
+//       // params to send additional data, for eg. AccessToken or userUserId
+//       let params = ["userID":"userId","accessToken":"your accessToken"]
+//       print(params)
+//       AF.upload(multipartFormData: { multiPart in
+//           for (key,keyValue) in params{
+//               if let keyData = keyValue.data(using: .utf8){
+//                   multiPart.append(keyData, withName: key)
+//               }
+//           }
+//           
+//           multiPart.append(imgData, withName: "key",fileName: imageName,mimeType: "image/*")
+//       }, to: "Your URL",headers: []).responseJSON { apiResponse in
+//           
+//           switch apiResponse.result{
+//           case .success(_):
+//               let apiDictionary = apiResponse.value as? [String:Any]
+//               print("apiResponse --- \(apiDictionary)")
+//           case .failure(_):
+//               print("got an error")
+//           }
+//       }
+//   }
     
 }
+
+
+//cose da chiedere a edo:
+//come gestire il fail durante il log in
+//fagli vedere il resto dell'app
+
+
+//cose da chiedere agli altri:
+//come posso fare la streak
+//cooperaimo per mandare l'immagine
